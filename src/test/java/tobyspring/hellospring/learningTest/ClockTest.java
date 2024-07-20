505dbba03a20201ea14e53be794ace6aee8e12f9
snlee <cisconamsu@gmail.com>
2024-07-20 14:46:12 +0900

    학습 테스트 수정 Clock의 systemDefaultZone() 을 사용함

---
+++ src/test/java/tobyspring/hellospring/learningTest/ClockTest.java
@@ -0,0 +1,28 @@
+package tobyspring.hellospring.learningTest;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class ClockTest {
+    // Clock을 이용해서 LocalDateTime.now를 가져온다
+    @Test
+    void clock() {
+        // 현재 위치한 시간대 기준시간
+        Clock clock = Clock.systemDefaultZone();
+
+        LocalDateTime dt1 = LocalDateTime.now(clock);
+        LocalDateTime dt2 = LocalDateTime.now(clock);
+
+        Assertions.assertThat(dt2).isAfter(dt1);
+    }
+
+    // Clock을 Test에서 사용할 때 원하는 시간을 지정해서 현재 시간을 가져올 수 있게 할 수 있는가?
+    @Test
+    void fixedClock() {
+
+    }
+}
